584d70d2f65453c2a1ec25338e102697a19abaef
Tanak <tanakorncn.159@gmail.com>
2023-11-23 19:51:46 +0700

    7.51 23

--- app/src/main/java/com/example/prototype02/cheatsheet1.java
+++ app/src/main/java/com/example/prototype02/cheatsheet1.java
@@ -3,12 +3,24 @@ package com.example.prototype02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class cheatsheet1 extends AppCompatActivity {
 
+    MaterialButton btnBack;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cheatsheet1);
+
+        btnBack = findViewById(R.id.btnBack);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/prototype02/courseSelection.java
+++ app/src/main/java/com/example/prototype02/courseSelection.java
@@ -14,8 +14,8 @@ public class courseSelection extends AppCompatActivity {
 
     MaterialButton btnBack;
     MaterialButton whatIsJava;
+    MaterialButton cheatSheetJava;
 
-    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +23,7 @@ public class courseSelection extends AppCompatActivity {
 
         btnBack = findViewById(R.id.btnBack);
         whatIsJava = findViewById(R.id.whatIsJava);
+        cheatSheetJava = findViewById(R.id.cheatSheetJava); // Assuming cheatSheetJava is the correct ID in your layout
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,6 +37,14 @@ public class courseSelection extends AppCompatActivity {
             public void onClick(View view) {
                 Intent i = new Intent(courseSelection.this, Article01.class);
                 startActivity(i);
+            }
+        });
+
+        cheatSheetJava.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent z = new Intent(courseSelection.this, cheatsheet1.class);
+                startActivity(z);
             }
         });
     }
